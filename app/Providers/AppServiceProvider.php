f9c26ecaed822463bf8582f3a5a6af29ecb10aca
Fernando Barrios <fernando@nika.agency>
2025-06-23 18:05:06 -0300

    maduro issue 6

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,9 +13,6 @@ class AppServiceProvider extends ServiceProvider
      */
         public function register(): void
         {
-            if ($this->app->environment('local')) {
-                $this->app->register(\NunoMaduro\Collision\Adapters\Laravel\CollisionServiceProvider::class);
-            }
         }
 
     /**
