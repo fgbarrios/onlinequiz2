bbfd78783331c77250d1da55d66912c10930392d
Fernando Barrios <fernando@nika.agency>
2025-06-23 18:11:00 -0300

    maduro issue 7

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,6 +13,9 @@ class AppServiceProvider extends ServiceProvider
      */
         public function register(): void
         {
+             if ($this->app->environment('local', 'testing')) {
+                $this->app->register(\Spatie\LaravelIgnition\IgnitionServiceProvider::class);
+            }
         }
 
     /**
